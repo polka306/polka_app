02126f907ccbacb95ed3544f141a7b2967012abc
polka306 <71255975+polka306@users.noreply.github.com>
2020-12-28 01:05:41 +0900

    add db

--- app/main.py
+++ app/main.py
@@ -1,10 +1,40 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 app = Flask(__name__)
+
+app.config.from_object(config)
+
+#ORM
+db.init_app(app)
+migrate.init_app(app, db)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/register/')
+def register():
+    return render_template('register.html')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(200), nullable=False)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User', backref=db.backref('item_set'))
+    item_type = db.Column(db.Integer, nullable=False)
+    item_name = db.Column(db.String(200), nullable=False)
+    item_reinforce_level = db.Column(db.Integer, nullable=False)
+    item_reinforce_chance = db.Column(db.Float, nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+
